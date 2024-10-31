a122472368473acfb5c68fe27473a5a0e477f7b3
rdrlaguna <rdr.laguna@gmail.com>
2024-10-31 08:10:01 +0100

    Print snake on canvas

--- static/snake.js
+++ static/snake.js
@@ -12,10 +12,29 @@ window.addEventListener('resize', function() {
     // Adjust canvas size when window is resized
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight; 
-
-    // Set context fill method to white
-    ctx.fillStyle = 'white';
-    // Draw a rectangle
-    ctx.fillRect(10, 10, 100, 200);
 })
 
+
+// Represent snake using coordinates
+let snake = [
+    {x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150},
+];
+
+// Draws snake parts
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokestyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+// Prints snake parts on canvas
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+drawSnake();
