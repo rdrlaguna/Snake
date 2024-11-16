e08ec17c02e0e73f85b864e33f3ff4b47643492e
rdrlaguna <rdr.laguna@gmail.com>
2024-11-16 09:52:57 +0100

    Create grid constants

---
+++ constants.js
@@ -0,0 +1,10 @@
+// Declare constants
+const CELL_SIZE = 55;
+const COLUMNS = 14;
+const ROWS = 16;
+const GAME = {
+    width: COLUMNS * CELL_SIZE,
+    height: ROWS * CELL_SIZE,
+    gameOver: false,
+    loop: 0,
+}
