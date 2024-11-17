c682344a78b5b20d677180141c1509d1d9894098
rdrlaguna <rdr.laguna@gmail.com>
2024-11-17 11:03:01 +0100

    Animate snake

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@ import {Snake} from "./snake.js"
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
+    // Canvas configuration
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = GAME.width;
@@ -11,6 +11,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const snake = new Snake(ROWS);
     snake.draw(ctx, CELL_SIZE);
 
-    console.log(snake);
+    function animate() {
+        // Clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw snake
+        snake.draw(ctx, CELL_SIZE)
+
+        // Change position
+        snake.update()
+    }
+
+    // Animate snake
+    GAME.loop = setInterval(animate, 250);
 
 })
